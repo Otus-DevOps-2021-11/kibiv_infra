6ef0d9048f22b3d8b0f6570148bc44283979b6f6
Ivan Kibitkin <IKibitkin@gmail.com>
2022-01-12 01:26:34 +0300

    add startup script HW deploy test app

--- startup_script.sh
+++ startup_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Install Ruby
-apt update
-apt install -y ruby-full ruby-bundler build-essential wget git 
+sudo apt update
+sudo apt install -y ruby-full ruby-bundler build-essential wget git 
 # Check Ruby
 ruby -v
 bundler -v
@@ -9,13 +9,13 @@ bundler -v
  wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
  echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 # Install MongoDB
- apt update
- apt-get install -y mongodb-org
+sudo  apt update
+sudo  apt install -y mongodb-org
 # Start service MongoDB
- systemctl start mongod
- systemctl enable mongod
+sudo  systemctl start mongod
+sudo  systemctl enable mongod
 # Check status service MongoDB
-systemctl status mongod
+sudo systemctl status mongod
 # Check Ruby
 pwd
 cd /home/yc-user
