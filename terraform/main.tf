fb6dcfb6ab898ca15b51a5662a29d9b58207184d
Ivan Kibitkin <IKibitkin@gmail.com>
2022-01-20 21:41:49 +0300

    Добавлен файл main.tf и определен провайдер

---
+++ terraform/main.tf
@@ -0,0 +1,6 @@
+provider "yandex" {
+  token     = "<AQAAAAAPL2RiAATuwf2p8j3So01TmCkmP7GV7XQа>"
+  cloud_id  = "<b1gjcabcuu2iqig18fnh>"
+  folder_id = "<b1gaitl24uq20bhcooso>"
+  zone      = "ru-central1-a"
+}
