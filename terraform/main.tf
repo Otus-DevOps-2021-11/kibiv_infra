bf311743ecd48c7e861a83016eb13eec9df91e82
Ivan Kibitkin <IKibitkin@gmail.com>
2022-01-25 20:15:31 +0300

    файлы подготвлены

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,14 @@
+# provider "yandex" {
+#   token     = "AQAAAAAPL2RiAATuwf2p8j3So01TmCkmP7GV7XQ"
+#   cloud_id  = "b1gjcabcuu2iqig18fnh"
+#   folder_id = "b1gaitl24uq20bhcooso"
+#   zone      = "ru-central1-a"
+# }
 provider "yandex" {
-  token     = "AQAAAAAPL2RiAATuwf2p8j3So01TmCkmP7GV7XQ"
-  cloud_id  = "b1gjcabcuu2iqig18fnh"
-  folder_id = "b1gaitl24uq20bhcooso"
-  zone      = "ru-central1-a"
+  token     = var.service_account_key_file
+  cloud_id  = var.cloud_id
+  folder_id = var.folder_id
+  zone      = var.zone
 }
 resource "yandex_compute_instance" "app" {
   name = "reddit-app"
@@ -11,20 +17,33 @@ resource "yandex_compute_instance" "app" {
     cores  = 2
     memory = 2
   }
-
   boot_disk {
     initialize_params {
       # Указать id образа созданного в предыдущем домашем задании
-      image_id = "fd8bgk6i049mnjr4add1"
+      image_id = var.image_id 
     }
   }
-
   network_interface {
     # Указан id подсети default-ru-central1-a
-    subnet_id = "e9bvhqah8bk7r97l9gag"
+    subnet_id = var.subnet_id
     nat       = true
   }
+  connection {
+  type = "ssh"
+  host = yandex_compute_instance.app.network_interface.0.nat_ip_address
+  user = "ubuntu"
+  agent = false
+  # путь до приватного ключа
+  private_key = "ubuntu:${file("var.private_key_path")}"
+  }
   metadata = {
-  ssh-keys = "ubuntu:${file("~/.ssh/ubuntu.pub")}"
+  ssh-keys = "ubuntu:${file(var.public_key_path)}"
+  }
+  provisioner "file" {
+  source = "files/puma.service"
+  destination = "/tmp/puma.service"
+  }
+  provisioner "remote-exec" {
+  script = "files/deploy.sh"
   }
 }
